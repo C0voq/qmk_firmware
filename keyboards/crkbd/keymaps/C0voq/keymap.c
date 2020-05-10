197c20615e43dc081f05916f0c4288a5f9c75294
Michael Densfelt <michael.densfelt@gmail.com>
2020-05-10 22:46:38 +0200

    kombostart

--- keyboards/crkbd/keymaps/C0voq/keymap.c
+++ keyboards/crkbd/keymaps/C0voq/keymap.c
@@ -30,6 +30,7 @@ enum macro_keycodes {
   KC_SAMPLEMACRO,
 };
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
